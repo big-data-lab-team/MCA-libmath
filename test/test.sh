897855cbaa359f34770aad607d996b2e172c2fa4
ali4006 <m_alari@encs.concordia.ca>
2020-11-15 11:04:40 -0500

    update test file

--- test/test.sh
+++ test/test.sh
@@ -13,16 +13,16 @@ func_bins=$2
 export LD_PRELOAD=${shared_lib}
 #export VFC_BACKENDS_SILENT_LOAD=False
 rm -f sigd*
-for p in 16;do
-    #export VFC_BACKENDS="libinterflop_ieee.so --debug"
-    export VFC_BACKENDS="libinterflop_mca.so --precision-binary64 ${p} --mode mca"
-    for fun in sinf cosf; do
+for p in 53;do
+    #export VFC_BACKENDS="libinterflop_ieee.so --debug" #--precision-binary64 ${p} --mode mca
+    export VFC_BACKENDS="libinterflop_mca.so -p ${p} -m mca"
+    for fun in sin cos; do
         rm -f points
         for x in $(seq -50 1 50); do
             echo $x >> points
             rm -f tmp
             for i in `seq 20`; do
-	        ${func_bins}/${fun} $x >> tmp
+	        ${func_bins}/bin/${fun} $x >> tmp
             done
             ./compute_sigdig.py tmp ${fun}-${p}
         done
